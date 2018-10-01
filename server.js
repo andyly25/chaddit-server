3bcee19d60fbbea86b1a86ea5168aef9d7451929
Andy Ly <andywindsong@gmail.com>
2018-10-01 00:06:21 -0700

    last attempt at corrs

--- server.js
+++ server.js
@@ -26,27 +26,27 @@ const app = express();
 app.use(morgan('common'));
 
 // CORS
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-  if (req.method === 'OPTIONS') {
-    return res.send(204);
-  }
-  next();
-});
-// app.use(cors({
-//   origin: 'https://chaddit-server.herokuapp.com/api',
-//   credentials: true,
-//   optionsSuccessStatus: 200
-// }));
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+//   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+//   if (req.method === 'OPTIONS') {
+//     return res.send(204);
+//   }
+//   next();
+// });
+app.use(cors({
+  origin: 'https://chaddit-server.herokuapp.com',
+  credentials: true,
+  optionsSuccessStatus: 200
+}));
 
-// // allowing preflight across the board
-// app.options('*', cors({
-//   origin: 'https://chaddit-server.herokuapp.com/api',
-//   credentials: true,
-//   optionsSuccessStatus: 200
-// }));
+// allowing preflight across the board
+app.options('*', cors({
+  origin: 'https://chaddit-server.herokuapp.com',
+  credentials: true,
+  optionsSuccessStatus: 200
+}));
 
 passport.use(localStrategy);
 passport.use(jwtStrategy);
