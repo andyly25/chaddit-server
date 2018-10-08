b491983006498b7a75421e3a175adbcf14424645
Andy Ly <andywindsong@gmail.com>
2018-10-08 16:36:45 -0700

    another attempt

--- config.js
+++ config.js
@@ -4,3 +4,12 @@ exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhos
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+
+// I decided to see if I can host the example code correctly first, but I've been having problems.
+// It works locally, so now I move on to hosting. 
+// For the node js code I hosted at Heroku and included a DATABASE_URL with my mlab in the Heroku settings. 
+// And for the Reactjs code I made it grab from the heroku app url: https://chaddit-server.herokuapp.com/api in the configs
+// At http://chaddit-client.surge.sh/ I can see it was able to display the login/register forms.
+// However if I were to try register or login I receive this 
+// Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource
+// the nodejs code already included some code for cors, and I tried using the cors package as well.
--- server.js.old.js
+++ server.js.old.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const passport = require('passport');
+const cors = require('cors');
 
 // Here we use destructuring assignment with renaming so the two variables
 // called router (from ./users and ./auth) have different names
@@ -24,16 +25,30 @@ const app = express();
 // Logging
 app.use(morgan('common'));
 
+app.use(cors());
+
 // CORS
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-  if (req.method === 'OPTIONS') {
-    return res.send(204);
-  }
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+//   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+//   if (req.method === 'OPTIONS') {
+//     return res.send(204);
+//   }
+//   next();
+// });
+// app.use(cors({
+//   origin: 'https://chaddit-server.herokuapp.com',
+//   credentials: true,
+//   optionsSuccessStatus: 200
+// }));
+
+// // allowing preflight across the board
+// app.options('*', cors({
+//   origin: 'https://chaddit-server.herokuapp.com',
+//   credentials: true,
+//   optionsSuccessStatus: 200
+// }));
 
 passport.use(localStrategy);
 passport.use(jwtStrategy);
